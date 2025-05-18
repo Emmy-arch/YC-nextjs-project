2320580dd6a251f38578913f847be31fc9202814
Emmy-arch <chiemetamossai66@gmail.com>
2025-05-18 16:54:38 +0100

    updating sanity write

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,52 @@
-import Image from "next/image";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const params = { search: query || null };
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="w-full bg-primary min-h-[530px] bg-pink-500 flex justify-center items-center flex-col py-10 px-6">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="px-6 py-10 max-w-7xl mx-auto">
+        <p className="font-semibold text-[30px] text-black">
+          {query ? `Search results for "${query}"` : "All startups"}
+        </p>
+
+        <ul className="mt-7 grid md:grid-cols-3 sm:grid-cols-2 gap-5">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="text-black-100 text-sm font-normal">
+              No startups found
+            </p>
+          )}
+        </ul>
+      </section>
+
+      <SanityLive />
     </>
   );
 }
---
+++ components/StartupCard.tsx
@@ -0,0 +1,85 @@
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Author, Startup } from "@/sanity/types";
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author,
+    title,
+    category,
+    _id,
+    description,
+    image,
+  } = post;
+  return (
+    <li className="bg-white border-[5px] border-black py-6 px-5 rounded-[22px] shadow-200 hover:border-pink transition-all duration-500 hover:shadow-300 hover:border-pink-600 hover:bg-pink-100 group">
+      <div className="flex justify-between items-center">
+        <p className="font-medium text-[16px] bg-pink-100 px-4 py-2 rounded-full group-hover:bg-white-100">
+          {formatDate(_createdAt)}
+        </p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-pink-600" />
+          <span className="text-16-radius">{views}</span>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${author?._id}`}>
+            <p className="font-medium text-[16px] text-black line-clamp-1">
+              {author?.name}
+            </p>
+          </Link>
+
+          <Link href={`/startup/${_id}`}>
+            <h3 className="font-semibold text-[26px] text-black line-clamp-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+        <Link href={`/user/${author?._id}`}>
+          <Image
+            // src="https://placehold.co/48x48"
+            src="https://placehold.co/48x48"
+            alt="placeholder"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`/startup/${_id}`}>
+        <p className="font-normal text-[16px] line-clamp-2 my-3 text-black-100 break-all">
+          {description}
+        </p>
+
+        <img
+          src={image}
+          alt="placeholder"
+          className="w-full h-[164px] rounded-[10px] object-cover"
+        />
+      </Link>
+
+      <div className="flex justify-between items-center gap-3 mt-5">
+        <Link href={`/?query=${category?.toLowerCase()}`}>
+          <p className="font-medium text-[16px] text-black">{category}</p>
+        </Link>
+        <Button
+          className="rounded-full bg-black font-medium text-[16px] text-white px-5 py-3"
+          asChild
+        >
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export default StartupCard;
