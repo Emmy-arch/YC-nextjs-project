aa2056b754b95d7f9ac3bb94179e5fd00841d6ff
Emmy-arch <chiemetamossai66@gmail.com>
2025-05-24 23:56:03 +0100

    replaced next.config.ts to next.config.js again

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
